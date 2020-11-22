aff1fdc93d6b2c2b786935f29a7768645f5b3c6e
Ezequiel Rabinovich <erabinovich@gmail.com>
2020-11-22 12:27:53 -0300

    initial commit

---
+++ scraper/__tests__/entries.test.js
@@ -0,0 +1,51 @@
+const nockUtils = require('nock-utils');
+const entries = require("../entries");
+const recorder = new nockUtils.HttpRecorder(`${__dirname}/cassettes/entries.json`);
+const league = 'Obscuria Potentis';
+const match = 50;
+
+describe('Entries Scraper', () => {
+    jest.setTimeout(60000);
+    beforeEach(() => recorder.start());
+    afterEach(() => recorder.stop());
+
+    it('scraps the home team name', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].home.team).toMatch(/^[a-z0-9 ]+$/i);
+    });
+
+    it('scraps the home team coach', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].home.coach).toMatch(/^[a-z0-9 ]+$/i);
+    });
+
+    it('scraps the away team name', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].away.team).toMatch(/^[a-z0-9 ]+$/i);
+    });
+
+    it('scraps the away team coach', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].away.coach).toMatch(/^[a-z0-9 ]+$/i);
+    });
+
+    it('scraps the result', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].result).toMatch(/^[0-9?] - [0-9?]$/);
+    });
+
+    it('scraps the id', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].id).toMatch(/^[\da-f]+$/i)
+    });
+
+    it('scraps the competition name', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].competition.name).toMatch(/^[a-z0-9 ]+$/i);
+    });
+
+    it('scraps the competition image', async () => {
+        const result = await entries(league);
+        expect(result.data.matches[match].competition.image).toMatch(/^https?:\/\/.*$/i);
+    });
+})
