4ef3c8f9433d8e77f0b471c6dc5c7ae8606e105d
Ezequiel Rabinovich <erabinovich@gmail.com>
2020-11-22 13:01:30 -0300

    refactor: improve entries interface

--- scraper/__tests__/entries.test.js
+++ scraper/__tests__/entries.test.js
@@ -1,51 +1,53 @@
-const nockUtils = require('nock-utils');
+const nockUtils = require("nock-utils");
 const entries = require("../entries");
-const recorder = new nockUtils.HttpRecorder(`${__dirname}/cassettes/entries.json`);
-const league = 'Obscuria Potentis';
+const recorder = new nockUtils.HttpRecorder(
+  `${__dirname}/cassettes/entries.json`
+);
+const league = "Obscuria Potentis";
 const match = 50;
 
-describe('Entries Scraper', () => {
-    jest.setTimeout(60000);
-    beforeEach(() => recorder.start());
-    afterEach(() => recorder.stop());
+describe("Entries Scraper", () => {
+  jest.setTimeout(60000);
+  beforeEach(() => recorder.start());
+  afterEach(() => recorder.stop());
 
-    it('scraps the home team name', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].home.team).toMatch(/^[a-z0-9 ]+$/i);
-    });
+  it("scraps the home team name", async () => {
+    const result = await entries(league);
+    expect(result[match].home.team).toMatch(/^[a-z0-9 ]+$/i);
+  });
 
-    it('scraps the home team coach', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].home.coach).toMatch(/^[a-z0-9 ]+$/i);
-    });
+  it("scraps the home team coach", async () => {
+    const result = await entries(league);
+    expect(result[match].home.coach).toMatch(/^[a-z0-9 ]+$/i);
+  });
 
-    it('scraps the away team name', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].away.team).toMatch(/^[a-z0-9 ]+$/i);
-    });
+  it("scraps the away team name", async () => {
+    const result = await entries(league);
+    expect(result[match].away.team).toMatch(/^[a-z0-9 ]+$/i);
+  });
 
-    it('scraps the away team coach', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].away.coach).toMatch(/^[a-z0-9 ]+$/i);
-    });
+  it("scraps the away team coach", async () => {
+    const result = await entries(league);
+    expect(result[match].away.coach).toMatch(/^[a-z0-9 ]+$/i);
+  });
 
-    it('scraps the result', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].result).toMatch(/^[0-9?] - [0-9?]$/);
-    });
+  it("scraps the result", async () => {
+    const result = await entries(league);
+    expect(result[match].result).toMatch(/^[0-9?] - [0-9?]$/);
+  });
 
-    it('scraps the id', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].id).toMatch(/^[\da-f]+$/i)
-    });
+  it("scraps the id", async () => {
+    const result = await entries(league);
+    expect(result[match].id).toMatch(/^[\da-f]+$/i);
+  });
 
-    it('scraps the competition name', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].competition.name).toMatch(/^[a-z0-9 ]+$/i);
-    });
+  it("scraps the competition name", async () => {
+    const result = await entries(league);
+    expect(result[match].competition.name).toMatch(/^[a-z0-9 ]+$/i);
+  });
 
-    it('scraps the competition image', async () => {
-        const result = await entries(league);
-        expect(result.data.matches[match].competition.image).toMatch(/^https?:\/\/.*$/i);
-    });
-})
+  it("scraps the competition image", async () => {
+    const result = await entries(league);
+    expect(result[match].competition.image).toMatch(/^https?:\/\/.*$/i);
+  });
+});
--- scraper/entries.js
+++ scraper/entries.js
@@ -1,58 +1,64 @@
-const scrapeIt = require('scrape-it');
+const scrapeIt = require("scrape-it");
 
-const url = league =>
-  `https://web.cyanide-studio.com/ws/bb2/?league=${league.replace(/ /g, '+')}&platform=pc&ajax=entries`;
+const url = (league) =>
+  `https://web.cyanide-studio.com/ws/bb2/?league=${league.replace(
+    / /g,
+    "+"
+  )}&platform=pc&ajax=entries`;
 
-module.exports = league => scrapeIt(url(league), {
-  matches: {
-    listItem: 'tr',
-    data: {
-      competition: {
-        data: {
-          name: {
-            selector: '.left > a',
-            eq: 0,
+const scrap = (league) =>
+  scrapeIt(url(league), {
+    matches: {
+      listItem: "tr",
+      data: {
+        competition: {
+          data: {
+            name: {
+              selector: ".left > a",
+              eq: 0,
+            },
+            image: {
+              selector: ".left > a > span > img",
+              attr: "src",
+              eq: 0,
+            },
           },
-          image: {
-            selector: '.left > a > span > img',
-            attr: 'src',
-            eq: 0,
-          }
-        }
-      },
-      home: {
-        data: {
-          coach: {
-            selector: '.right > a',
-            eq: 0
+        },
+        home: {
+          data: {
+            coach: {
+              selector: ".right > a",
+              eq: 0,
+            },
+            team: {
+              selector: ".right > a",
+              eq: 1,
+            },
           },
-          team: {
-            selector: '.right > a',
-            eq: 1
-          }
-        }
-      },
-      away: {
-        data: {
-          coach: {
-            selector: '.left > a',
-            eq: 3
+        },
+        away: {
+          data: {
+            coach: {
+              selector: ".left > a",
+              eq: 3,
+            },
+            team: {
+              selector: ".left > a",
+              eq: 2,
+            },
           },
-          team: {
-            selector: '.left > a',
-            eq: 2
-          }
-        }
+        },
+        result: {
+          selector: ".center",
+          eq: 1,
+        },
+        id: {
+          selector: ".center > a",
+          attr: "data",
+          eq: 1,
+        },
       },
-      result: {
-        selector: '.center',
-        eq: 1
-      },
-      id: {
-        selector: '.center > a',
-        attr: 'data',
-        eq: 1
-      }
-    }
-  }
-});
+    },
+  });
+
+module.exports = async (league) => (await scrap(league)).data.matches.slice(1);
